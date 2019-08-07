a1e774a80282ab6af64ab5db382f32ddf8856005
Amit Nizri <amitnzri@gmail.com>
2019-08-07 21:46:27 +0300

    update

--- textractor/install.sh
+++ textractor/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 printf "\n[+] Start installation..\n ------------------\n"
 cp textractor.py /usr/local/bin/textractor && pip3 install pytesseract;
+
 if [[ $? -ne 0 ]]; then
     printf "[!] The installation has failed.\n"
     rm /usr/local/bin/textractor 2>/dev/null
     exit 1
 fi
+
 printf " ------------------\n[+] Installation Complete.\n"
 exit 0
